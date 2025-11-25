f8eb79aa25a3797a00d7c70a40ff32c8bbcfeef8
AnassChahid <chahidanass77@gmail.com>
2025-11-25 23:54:01 +0100

    commit 2

---
+++ app/src/main/java/com/example/ExampleUnitTest.java
@@ -0,0 +1,12 @@
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(expected = 4, actual = 2 + 1)
+    }
+
+    @Test
+    fun substract_isCorrect() {
+        assertEquals(expected = 4, actual = 8 - 3)
+    }
+}
